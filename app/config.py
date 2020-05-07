9d9c70ffb4025823309189552b9772e2ccee679a
Liza <eelisn@mail.ru>
2020-05-07 04:33:50 +0300

    commit update

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 import pymysql
+import os 
 
 class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Htyg-htyg1$@localhost/test1'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -3,15 +3,28 @@ from flask import render_template
 from models import Post, Tag
 from flask import request
 from .forms import PostForm
+from app import db
+from flask import redirect, url_for
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
-posts.route('/create')
+@posts.route('/create', methods=['POST', 'GET'])
 def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body = body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Что-то пошло не так')
+        return redirect(url_for('posts.index'))
     form = PostForm()
-    return render_template('post/create_post.html', form=form)
+    return render_template('posts/create_post.html', form=form)
 
 
 @posts.route('/')
@@ -20,7 +33,7 @@ def index():
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
     else:
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.created.desc())
     return render_template('posts/index.html', posts=posts)
 
 
